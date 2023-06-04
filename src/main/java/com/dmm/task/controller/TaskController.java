d75657b3f0ab20e9e93adbd70b6d7feaa224502c
YasukoDmm <112673301+yurihoyu@users.noreply.github.com>
2023-06-04 14:43:39 +0900

    カレンダー表示質問

--- src/main/java/com/dmm/task/controller/TaskController.java
+++ src/main/java/com/dmm/task/controller/TaskController.java
@@ -1,18 +1,16 @@
 package com.dmm.task.controller;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
-import com.dmm.task.data.entity.Tasks;
 import com.dmm.task.data.repository.TasksRepository;
-
-
 
 @Controller
 public class TaskController {
@@ -20,46 +18,66 @@ public class TaskController {
 	@Autowired
 	private TasksRepository repo;
 
-
 	/**
-	 * 投稿の一覧表示.
+	 * カレンダーの表示.
 	 *
 	 * @param model モデル
 	 * @return 遷移先
 	 */
 
+	@GetMapping("/main")
+	public String main(Model model) {
+		// 1. 2次元表になるので、ListのListを用意する
+		List<List<LocalDate>> month = new ArrayList<>();
 
+		// 2. 1週間分のLocalDateを格納するListを用意する
+		List<LocalDate> week = new ArrayList<>();
 
-	    @GetMapping("/main")
-	    public String main() {
-	        return "main";
-	    }
+		// 3. その月の1日のLocalDateを取得する
+		//別パターン
+//		LocalDate d = LocalDate.now().withDayOfMonth(1);
 
+		LocalDate day;
+		day = LocalDate.now();
+		//その月の1日のLocalDate
+		day = LocalDate.of(day.getYear(), day.getMonthValue(), 1);
 
+		// 4-1. 1日の曜日を表すDayOfWeekを取得
+		DayOfWeek w = day.getDayOfWeek();
 
-		/**
-		 * 投稿の一覧表示.
-		 *
-		 * @param model モデル
-		 * @return 遷移先
-		 */
-		@PostMapping("/main")
-		public String posts(Model model) {
-			// 逆順で投稿をすべて取得する
-			List<Tasks> list = repo.findAll(Sort.by(Sort.Direction.DESC, "id"));
-//	    Collections.reverse(list); //普通に取得してこちらの処理でもOK
-			model.addAttribute("tasks", list);
+		// 4-2. 上で取得した1日のLocalDateにその曜日の値（DayOfWeek#getValue 1～7)をマイナスして前月分のLocalDateを求める
+		//1日の週の月曜日のLocalDateがわかる
+		day = day.minusDays(w.getValue());
 
-			return "/main";
+		// 5. 1日ずつ増やしてLocalDateを求めていき、2．で作成したListへ格納していき、1週間分詰めたら1．のリストへ格納する
+		for (int i = 1; i <= 7; i++) {
+			day = day.plusDays(1);
+			week.add(day);
+		}
+
+		month.add(week);
+		week = new ArrayList<>();    // 次週分のリストを用意
+
+		// 6. 2週目以降は単純に1日ずつ日を増やしながらLocalDateを求めてListへ格納していき、
+		//土曜日になったら1．のリストへ格納して新しいListを生成する
+		//（月末を求めるにはLocalDate#lengthOfMonth()を使う）
+
+		for(int i = 7; i <= day.lengthOfMonth(); i++) {
+			for (int j = 1; j <= 7; j++) {
+				day = day.plusDays(1);
+				week.add(day);
+			}
+
+			month.add(week);
+			week = new ArrayList<>();    // 次週分のリストを用意
+
 		}
 
 
+		model.addAttribute("matrix", month);
 
-
-
-
-
-
+		return "main";
+	}
 
 
 
